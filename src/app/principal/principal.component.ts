cac10974b62071beda5da09732dbccf7fb8fe971
felipe <henry19952009@hotmail.com>
2024-01-29 19:06:43 -0500

    carrusel completo

--- src/app/principal/principal.component.ts
+++ src/app/principal/principal.component.ts
@@ -9,11 +9,5 @@ import { NavbarComponent } from '../navbar/navbar.component';
   styleUrl: './principal.component.css'
 })
 export class PrincipalComponent {
-  angle:number = 0;
-  spinner:any;
-  galleryspin(sign:number):void{
-  this.spinner = document.querySelector("#spinner");
-  if (!sign) { this.angle = this.angle + 45; } else { this.angle = this.angle - 45; }
-  this.spinner.setAttribute("style","-webkit-transform: rotateY("+ this.angle +"deg); -moz-transform: rotateY("+ this.angle +"deg); transform: rotateY("+ this.angle +"deg);");
-  }
+
 }
